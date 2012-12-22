25a9bf5c58bbb0789b4f1ad4ba8572bf18b513c7
nigue <anibaldavor@gmail.com>
2012-12-22 19:14:49 -0300

    avances web

--- src/main/java/cl/utem/informatica/alumnos/nigue/jsf/pages/SeeUserBean.java
+++ src/main/java/cl/utem/informatica/alumnos/nigue/jsf/pages/SeeUserBean.java
@@ -1,15 +1,35 @@
 package cl.utem.informatica.alumnos.nigue.jsf.pages;
 
+import cl.utem.informatica.alumnos.nigue.modelo.Usuario;
 import cl.utem.informatica.alumnos.nigue.servicio.UsuarioService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import org.apache.log4j.Logger;
 
 public class SeeUserBean {
 
     private static final Logger LOGGER = Logger.getLogger(SeeUserBean.class);
-    
-    @Resource(name = "usuariosService")
-    private transient UsuarioService usuariosService;
-    
-    
+    @Resource(name = "usuarioService")
+    private transient UsuarioService usuarioService;
+    private List<Usuario> usuarios = new ArrayList<Usuario>();
+
+    @PostConstruct
+    public void initPostDeclarative() {
+
+        LOGGER.debug("######## LIST[Usuario] #######");
+        List<Usuario> usuariosDao = usuarioService.getAllUsuario();
+        for (Usuario usuario : usuariosDao) {
+            this.getUsuarios().add(usuario);
+        }
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
 }
