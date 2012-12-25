c086827250aabbcd2718b21904c9966735f03e92
nigue <anibaldavor@gmail.com>
2012-12-25 01:48:14 -0300

    avances

--- src/main/java/cl/utem/informatica/alumnos/nigue/jsf/pages/AddUserBean.java
+++ src/main/java/cl/utem/informatica/alumnos/nigue/jsf/pages/AddUserBean.java
@@ -2,52 +2,74 @@ package cl.utem.informatica.alumnos.nigue.jsf.pages;
 
 import cl.utem.informatica.alumnos.nigue.servicio.UsuarioService;
 import javax.annotation.Resource;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import org.apache.log4j.Logger;
 
 public class AddUserBean {
-
+    
     private static final Logger LOGGER = Logger.getLogger(AddUserBean.class);
     @Resource(name = "usuarioService")
     private transient UsuarioService usuarioService;
-    private int rut;
-    private String password;
+    private int rut = 16978752;
+    private String password = "asdf";
     
     public void rutValidator(
             FacesContext context,
             UIComponent component,
             Object value) throws ValidatorException {
         LOGGER.debug("######## VALIDATION ########");
-
-        String newRut = (String) value;
-//        List<Player> players = playerDAO.findByField("name", playerData);
-//        if (!players.isEmpty()) {
-//            LOGGER.debug(playerData + " is already on DB");
-//            throw new ValidatorException(new FacesMessage(
-//                    FacesMessage.SEVERITY_WARN,
-//                    playerData + " is already on DB",
-//                    "WARNING"));
-//        }
+                      
+//                throw new ValidatorException(new FacesMessage(
+//                        FacesMessage.SEVERITY_WARN,
+//                        "error",
+//                        "WARNING"));  
+        try {
+            String[] varRut = (String[]) value;
+            boolean rutValido = verificarRut(new Integer(varRut[0]), varRut[1].charAt(0));
+            if (!rutValido) {
+                String msg = varRut[0] + "-" + varRut[1] + ": no es un rut válido";
+                LOGGER.debug(msg);
+                throw new ValidatorException(new FacesMessage(
+                        FacesMessage.SEVERITY_WARN,
+                        msg,
+                        "WARNING"));
+            }
+        } catch (Exception e) {
+            LOGGER.debug(e.getMessage());
+        }
     }
     
-     public String doSubmit() {
-         return "hola";
-     }
-
+    public boolean verificarRut(int rut, char dv) {
+        LOGGER.debug("verirficar rut");
+        int m = 0, s = 1;
+        for (; rut != 0; rut /= 10) {
+            s = (s + rut % 10 * (9 - m++ % 6)) % 11;
+        }
+        boolean result = ( dv == (char) (s != 0 ? s + 47 : 75));
+        LOGGER.debug(result);
+        return result;
+    }
+    
+    public String doSubmit() {
+        LOGGER.debug("######## DO SUBMIT ########");
+        return "hola";
+    }
+    
     public int getRut() {
         return rut;
     }
-
+    
     public void setRut(int rut) {
         this.rut = rut;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
--- src/main/java/cl/utem/informatica/alumnos/nigue/jsf/utils/converter/RutConverter.java
+++ src/main/java/cl/utem/informatica/alumnos/nigue/jsf/utils/converter/RutConverter.java
@@ -15,16 +15,34 @@ public class RutConverter implements Converter {
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        LOGGER.debug("########### Entrando a convertidor ##########");
-        throw new ValidatorException(new FacesMessage(
-                    FacesMessage.SEVERITY_WARN,
-                    value + " is already on DB",
-                    "WARNING"));
-//        return null;
+        LOGGER.debug("########### Entrando a convertidor: rut a String[] ##########");
+        String[] rut = null;
+        try {
+            rut = value.split("-");
+        } catch (Exception e) {
+            LOGGER.debug(e.getMessage());
+//        throw new ValidatorException(new FacesMessage(
+//                    FacesMessage.SEVERITY_WARN,
+//                    value + " is already on DB",
+//                    "WARNING"));
+        }
+        return rut;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        return value.toString();
+        Integer rut = (Integer) value;
+        return rut + "-" + digitoVerificadorRut(rut);
+    }
+
+    public char digitoVerificadorRut(int rut) {
+        LOGGER.debug("digitoVerificadorRut");
+        int m = 0, s = 1;
+        for (; rut != 0; rut /= 10) {
+            s = (s + rut % 10 * (9 - m++ % 6)) % 11;
+        }
+        char dv = (char) (s != 0 ? s + 47 : 75);
+        LOGGER.debug(" es: " + dv);
+        return dv;
     }
 }
