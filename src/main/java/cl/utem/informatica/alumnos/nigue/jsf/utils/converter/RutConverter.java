9bcf26c318233b3f1632e6db968e1b852b665248
nigue <anibaldavor@gmail.com>
2012-12-29 15:13:03 -0300

    empezando 2

--- src/main/java/cl/utem/informatica/alumnos/nigue/jsf/utils/converter/RutConverter.java
+++ src/main/java/cl/utem/informatica/alumnos/nigue/jsf/utils/converter/RutConverter.java
@@ -1,11 +1,9 @@
 package cl.utem.informatica.alumnos.nigue.jsf.utils.converter;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
-import javax.faces.validator.ValidatorException;
 import org.apache.log4j.Logger;
 
 @FacesConverter("rut")
@@ -24,11 +22,16 @@ public class RutConverter implements Converter {
             dataNum = dataNum.replaceAll("\\.", "");
             Integer number = new Integer(dataNum);
             char dv = dataRut[1].charAt(0);
-            LOGGER.debug("rut completo: " + number + "-" + dv);
-            if (!verificarRut(number, dv)) {
+            String dvData = "" + dv;
+            dvData = dvData.toUpperCase();
+            LOGGER.debug("rut completo: " + number + "-" + dvData);
+            LOGGER.debug("dv completo: " + digitoVerificadorRut(number));
+            LOGGER.debug("paso: " + dvData.equals(digitoVerificadorRut(number).toString()));
+            if (dvData.equals(digitoVerificadorRut(number).toString())) {
+                return number;
+            } else {
                 return null;
             }
-            return number;
         } catch (Exception e) {
             LOGGER.debug(e);
             return null;
@@ -54,15 +57,14 @@ public class RutConverter implements Converter {
         }
     }
 
-    public char digitoVerificadorRut(int rut) {
-        LOGGER.debug("digitoVerificadorRut");
+    public String digitoVerificadorRut(int rut) {
         int m = 0, s = 1;
         for (; rut != 0; rut /= 10) {
             s = (s + rut % 10 * (9 - m++ % 6)) % 11;
         }
         char dv = (char) (s != 0 ? s + 47 : 75);
-        LOGGER.debug(" es: " + dv);
-        return dv;
+        String result = "" + dv;
+        return result;
     }
 
     public boolean verificarRut(int rut, char dv) {
