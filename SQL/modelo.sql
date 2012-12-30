d6608318539af3590dd49f2c89f77587ef4b7095
Sebastián Salazar Molina <sebasalazar@gmail.com>
2012-12-29 22:10:58 -0300

    Cambios en el modelo de Base de datos

--- SQL/modelo.sql
+++ SQL/modelo.sql
@@ -2,25 +2,22 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS usuarios CASCADE;
 CREATE TABLE usuarios (
-        rut int NOT NULL,
-        password varchar(40) NOT NULL,
-        PRIMARY KEY (rut)
+	pk serial NOT NULL,
+	rut int NOT NULL,
+	nombre varchar(255) NOT NULL,
+	password varchar(40) NOT NULL,
+	UNIQUE (rut),
+	PRIMARY KEY (pk)
 );
-
-INSERT INTO usuarios VALUES (16567393, 'user_1');
-INSERT INTO usuarios VALUES (24525711, 'user_2');
-INSERT INTO usuarios VALUES (16659132, 'user_3');
-INSERT INTO usuarios VALUES (20322478, 'user_4');
 
 DROP TABLE IF EXISTS accesos CASCADE;
 CREATE TABLE accesos ( 
-        id SERIAL NOT NULL,
-        usuario int NOT NULL,
-        ip varchar(16) NOT NULL,
-        fecha timestamp NOT NULL,
-        PRIMARY KEY (id)
+	pk bigserial NOT NULL,
+	usuario_fk int NOT NULL REFERENCES usuarios(pk) ON UPDATE CASCADE ON DELETE CASCADE,
+	ip inet NOT NULL,
+	fecha timestamptz NOT NULL,
+	PRIMARY KEY (pk)
 );
 
-INSERT INTO accesos (usuario, ip, fecha) VALUES (2234, '120.0.0.1', '2004-10-19 15:23:54');
 
 COMMIT;
